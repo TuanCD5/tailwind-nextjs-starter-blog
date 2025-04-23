9996db0d57fe79c05fdac46fcbfac861de8c6476
TuanCD5 <duongtuan.vnu.eng@gmail.com>
2025-04-23 17:19:08 +0700

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/work experience', title: 'Work Experience' },
   { href: '/about', title: 'About' },
 ]
 
